b909b6cd3aae1d4bc4a71c463d2ebe0b534a108a
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-22 16:07:26 +0200

    Add append to images

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 const lampImageEl = document.getElementById("lamp");
-const switcherEl = document.getElementById("button-switch");
+const lampImageOnEl = document.getElementById("light-on");
+const buttonSwitchEl = document.getElementById("button-switch-light");
 
-let img = document.createElement("img");
-img.src = "img/white_lamp.png";
+const imgLamp = document.createElement("img");
+imgLamp.src = "img/white_lamp.png";
+imgLamp.classList.add("image-fluid");
+console.log(imgLamp);
 
-document.body.appendChild(img);
+lampImageEl.append(imgLamp);
+
+const imgLightOn = document.createElement("img");
+imgLightOn.src = "img/yellow_lamp.png";
+imgLightOn.classList.add("image-fluid");
+console.log(imgLightOn);
+
+lampImageOnEl.append(imgLightOn);
+
+document.body.appendChild(imgLamp);
+
+buttonSwitchEl.addEventListener("click", () => {
+  imgLamp.classList.toggle(imgLightOn);
+});
