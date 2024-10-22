bf523795b3fdcd39bc5be034c4c9e44853dee4ae
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-22 14:40:16 +0200

    Add img

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 const lampImageEl = document.getElementById("lamp");
 const switcherEl = document.getElementById("button-switch");
+
+let img = document.createElement("img");
+img.src = "img/white_lamp.png";
